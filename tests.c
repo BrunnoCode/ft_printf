056a8bcce60cd927e8a30c19aa2db5cca4c7943d
bbotelho <brunnowr.bw@gmail.com>
2023-10-25 22:36:11 +0200

    nós conseguiremos, confie na capacidade que Deus te deu

--- tests.c
+++ tests.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 12:30:35 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/25 15:46:54 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/10/25 21:52:50 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 int	main(void)
 {
-	// ft_printf("mi funcion: %%\n");
-	// ft_printf("mi funcion: %x %d %i \n", -455, 455, -455);
-	printf("%ld", ft_power(2, 3));
+	char const *form = "hola que tal";
+	printf("resultado: %s\n %s\n", *(++form));
 
 	return (0);
 }
