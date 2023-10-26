cbfda15e1e68b9d66539845963c26b6332bfe05f
Brunno Warley Botelho Rocha <bbotelho@car5s5.42barcelona.com>
2023-10-26 11:00:32 +0200

    gracias Dios por un angel

--- ft_printf.c
+++ ft_printf.c
@@ -1,104 +1,92 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   esbozo.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/20 14:49:28 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/25 16:01:03 by bbotelho         ###   ########.fr       */
+/*   Created: 2023/10/25 20:20:14 by bbotelho          #+#    #+#             */
+/*   Updated: 2023/10/26 11:00:10 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-long ft_power(int base, int exponente)
+void	ft_putchr(int c, int *datap)
 {
-	int i = 0;
-	int resultado = 1;
-	while(i < exponente)
+	if(*datap != -1)
 	{
-		resultado *= base;
-		i++;		
+		if(write(1, &c, 1) == -1)
+			*datap = -1;
+		else
+			*datap += 1;	
 	}
-	return (resultado);
 }
-int ft_putnhex(unsigned long n, char *base, int nbase)
+void	ft_str(char *str, int *datap)
 {
-		 
-}
-
-
-
-int	ft_print_char(int c)
-{	
-	return(write(1, &c, 1));
-}
-
-int	ft_print_str(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (*str)
-	{
-		write(1, str, 1);
-		++i;
-		++str;
-	}
-	return (i);
-}
-
-int	ft_process(char format, va_list ptr)
-{
-	int	count;
-
-	count = 0;
-	if (format == '%')
-		count = ft_print_str("%");
-	else if (format == 'c')
-		count = ft_print_char(va_arg(ptr, int));
-	else if (format == 's')
-		count = ft_print_str(va_arg(ptr, char *));
-	// else if (format == 'd' || format == 'i')
-	// 	count = ft_digit(va_arg(ptr, int));
-	// else if (format == 'x' || format == 'X')
-	// 	count = ft_puthex(va_arg(ptr, long int), format, 16);
-	// else if(format == 'p')
-	// 	count = ft_pointer(va_arg(ptr, unsigned long), 16);
-	if(count == -1)
-		return (-1);
-	return (0);
-}
-
-int	ft_printf(char const *format, ...)
-{
-	va_list	ptr;
-	int		res;
-	int		i;
-	int		aux;
-
-	i = 0;
-	res = 0;
-	aux = 0;
-	va_start(ptr, format);
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
+	if (!str)
+		ft_str("(null)", datap);
+	else
+		while (*str)
 		{
-			i++;
-			aux = res;
-			res += ft_process(format[i], ptr);
-			if (aux > res)
-				return (-1);
+			ft_putchr(*str, datap);
+			str++;
+		}
+}
+
+void	ft_number(unsigned long n, int *datap, unsigned long base, char *basestr)
+{
+	if(n < 0)
+	{
+		ft_putchr('-', datap);
+		n = -n;
+	}	
+	if(n >= base)
+		ft_number(n  / base, datap, base, basestr);
+	if(*datap == -1)
+		return ;
+	ft_putchr(basestr[n % base], datap);
+}
+
+void	ft_formats(const char *formato, va_list ap, int *datap)
+{
+	if (*formato == '%')
+		ft_putchr('%', datap);		
+	else if (*formato == 'c')
+		ft_putchr(va_arg(ap, int), datap);		
+	else if (*formato == 's')
+		return (ft_str(va_arg(ap, char *), datap));
+	else if (*formato == 'd' || *formato == 'i')
+		ft_number(va_arg(ap, int), datap, 10, "0123456789");
+	else if (*formato == 'u')
+		ft_number(va_arg(ap, unsigned int), datap, 10, "0123456789");
+	else if (*formato == 'x')
+		ft_number(va_arg(ap, unsigned int), datap, 16, "0123456789abcdef");
+	else if (*formato == 'X')
+		ft_number(va_arg(ap, unsigned int), datap, 16, "0123456789ABCDEF");
+	else if (*formato == 'p')
+	{
+		ft_str("0x", datap);
+		ft_number(va_arg(ap, unsigned long), datap, 16, "0123456789abcdef");
+	}
+}
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		dataprinted;
+
+	dataprinted = 0;
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			ft_formats(++format, ap, &dataprinted);
 		}
 		else
-		{
-			if (write(1, &format[i], 1) == -1)
-				return (-1);
-		}
-		i++;
+			ft_putchr(*format, &dataprinted);
+		format++;
 	}
-	va_end(ptr);
-	return (res);
+	va_end(ap);
+	return (dataprinted);
 }
